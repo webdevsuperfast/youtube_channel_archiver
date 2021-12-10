d9b55ab2c6f903ac882f3acd5694bcdf14e58aaf
Rotsen Mark Acob <acob.rotsen@hotmail.com>
2021-12-10 08:47:13 +0800

    Added conditions

--- download_archive.sh
+++ download_archive.sh
@@ -1,7 +1,19 @@
 #!/bin/sh
+# Create a file if youtube-dl-playlists.txt doesnt exist
+if [ ! -f youtube-dl-playlists.txt ]; then
+  touch youtube-dl-playlists.txt
+fi
+
+# Get input from user
 echo Enter video URL
 read videourl
 
-echo "$videourl" >> youtube-dl-playlists.txt
+# Check if videourl is already in the file
+if grep -R "$videourl" youtube-dl-playlists.txt; then
+  echo "Video already in file"
+else
+  echo "$videourl" >> youtube-dl-playlists.txt
+fi
 
+# Execute command
 yt-dlp --config-location youtube-dl.conf
